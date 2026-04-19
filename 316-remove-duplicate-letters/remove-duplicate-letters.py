75e64680fb4ca34053bb66b6433e53b74b4f081a
mohansai1807 <saikmohan1@gmail.com>
2026-04-19 11:53:48 +0530

    Time: 7 ms (9.62%) | Memory: 19.4 MB (22.57%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/remove-duplicate-letters.py
@@ -2,8 +2,10 @@ class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         result = []
         seen = []
+        
         last_index = [0]*26
         n = len(s)
+
         for i in range(n):
             last_index[ord(s[i])-ord('a')] = i
         
@@ -11,11 +13,14 @@ class Solution:
             ch = s[i]
             if ch in seen:
                 continue
+
             while(result and ch < result[-1] and last_index[ord(result[-1]) - ord('a')]>i):
                 removed = result.pop()
                 seen.remove(removed)
+
             result.append(ch)
             seen.append(ch)
+
         return "".join(result)
                 
         
