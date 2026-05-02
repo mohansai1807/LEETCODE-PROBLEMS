8808ada7e17e1f0463f6b0038ec4184441700e35
mohansai1807 <saikmohan1@gmail.com>
2026-05-02 10:42:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (8.66%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,13 +1,30 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            int[] array = matrix[i];
-            for(int j : array){
-                if(j==target){
-                    return true;
-                }
+        // for(int i=0;i<matrix.length;i++){
+        //     int[] array = matrix[i];
+        //     for(int j : array){
+        //         if(j==target){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        int row = 0;
+        int col = matrix[0].length -1;
+
+        while(row< matrix.length && col>=0){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col]  > target){
+                col--;
+            }
+            else{
+                row++;
             }
         }
+
         return false;
     }
 }
